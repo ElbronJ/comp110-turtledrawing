8f4915e4d7ea8589aeb8d5bc2dfebd965fd23e06
Elbron Jackob <ejackob@sandiego.edu>
2023-02-08 14:04:44 -0800

    Elbron's name drawer

--- draw_polygon.py
+++ draw_polygon.py
@@ -9,8 +9,15 @@ import turtle
 # create a turtle and set the pen color
 duzzy = turtle.Turtle()
 duzzy.pencolor("red")
+#ask user the number of sides of the polygon and the length
+number_sides = int(input("Please enter the amount of sides"))
+length_of_each_side = int(input("Please enter the length of each side"))
+#draw polygon
+for i in range(number_sides):
+    duzzy.forward(length_of_each_side)
+    duzzy.left(360/number_sides)
 
-# PUT YOUR NEW CODE HERE
+
 
 # keep the turtle window open until we click on it
 turtle_window = turtle.Screen()
--- loopy_pentagon.py
+++ loopy_pentagon.py
@@ -6,21 +6,27 @@ Program to draw a pentagon using turtles.
 
 import turtle
 
-# create a turtle and set the pen color
+# create a turtle, have user input turtle color and size
 sat = turtle.Turtle()
-sat.pencolor("red")
-
+color_input = input('Please choose a color for the turtle')
+turtle_size = int(input("Please indicate how far the turtle should go")) 
+sat.color(color_input)
+#draw the pentagon
+for i in range(5):
+    sat.forward(turtle_size)
+    sat.left(72)
+    
 # draw the pentagon
-sat.forward(50)
-sat.left(72)
-sat.forward(50)
-sat.left(72)
-sat.forward(50)
-sat.left(72)
-sat.forward(50)
-sat.left(72)
-sat.forward(50)
-sat.left(72)
+# sat.forward(50)
+# sat.left(72)
+# sat.forward(50)
+# sat.left(72)
+# sat.forward(50)
+# sat.left(72)
+# sat.forward(50)
+# sat.left(72)
+# sat.forward(50)
+# sat.left(72)
 
 # keep the turtle window open until we click on it
 turtle_window = turtle.Screen()
